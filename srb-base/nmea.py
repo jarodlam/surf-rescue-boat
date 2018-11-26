52a60f25eecaf5eddb053e72b334a4dfc0e64985
Jarod Lam <jlam96@gmail.com>
2018-11-26 14:35:10 +1000

    Python implementation of NMEA

---
+++ srb-base/nmea.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+#import re
+
+class Nmea:
+    
+    def __init__(self):
+        self.sentence = "$"
+    
+    # Append string to end of sentence.
+    def append(self, s):
+        if len(self.sentence) > 0:
+            if self.sentence[-1] != '$':
+                self.sentence = self.sentence + ','
+        
+        self.sentence = self.sentence + s
+    
+    # Generate checksum for the sentence.
+    def generateChecksum(self):
+        checksum = 0
+        
+        for c in self.sentence.encode():
+            if c == '$'.encode()[0]: continue
+            if c == '*'.encode()[0]: break
+            checksum ^= c
+        
+        checksumString = "%X" % checksum
+        return checksumString
+        
+    # Append checksum to the sentence.
+    def appendChecksum(self):
+        checksum = self.generateChecksum()
+        self.append('*' + checksum)
+    
+    # Check if the sentence is valid.
+    def validate(self):
+        if self.sentence[0]  != '$': return False
+        if self.sentence[-3] != '*': return False
+        
+        cs_recv = self.sentence[-2:]
+        cs_calc = self.generateChecksum()
+        if (cs_recv != cs_calc): return False
+        
+        return True
+        
+    # Return arguments as a list.
+    def listify(self):
+        sentenceStripped = self.sentence[self.sentence.find("$")+1:self.sentence.find("*")]
+        listified = sentenceStripped.split(',')
+        return listified
--- srb-base/srb-base.py
+++ srb-base/srb-base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import serial, sys
+import serial, sys, threading
+from nmea import *
 
 # Open the port
 try:
@@ -9,12 +10,30 @@ except:
     raise Exception('Please run the program with a valid serial port.')
 
 print("Serial port opened at %s" % ser.name)
+   
+# Serial input 
+def readSerial():
+    while True:
+        ser.write(b"hello")
+        line = ser.readline()
+        if len(line) > 0:
+            parseSentence(line)
 
-# Decode NMEA strings
+def parseSentence(sent):
+    # Create a Nmea object and validate
+    s = Nmea()
+    s.sentence = sent.decode().rstrip('\r\n')
+    if s.validate():
+        print("Valid sentence: %s" % s.sentence)
+    else:
+        print("Invalid sentence: %s" % s.sentence)
+
+# Thread
+serialThread = threading.Thread(target=readSerial)
+serialThread.daemon = True
+serialThread.start()
 
 while True:
-    ser.write(b"hello")
-    line = ser.readline()
-    print("%s" % line)
+    pass
 
 ser.close()
